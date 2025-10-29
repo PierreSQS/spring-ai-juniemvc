fbe2d31a148b7cb38eee2ef317dc3f933983c086
PierreSQS <nomo_messina@web.de>
2025-10-30 00:50:50 +0100

    Sec10_Chap76-XXX: optimized test code

--- src/test/java/guru/springframework/spring_ai_juniemvc/SpringAiJuniemvcApplicationTests.java
+++ src/test/java/guru/springframework/spring_ai_juniemvc/SpringAiJuniemvcApplicationTests.java
@@ -2,12 +2,16 @@ package guru.springframework.spring_ai_juniemvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringAiJuniemvcApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext appCtx) {
+        assertThat(appCtx).isNotNull();
 	}
 
 }

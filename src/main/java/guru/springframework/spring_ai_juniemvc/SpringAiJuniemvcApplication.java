cd01567556a1420774a3fa410edcc7767d98aed7
PierreSQS <nomo_messina@web.de>
2025-10-30 00:42:35 +0100

    Sec10_Chap76-XXX: initial commit

---
+++ src/main/java/guru/springframework/spring_ai_juniemvc/SpringAiJuniemvcApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring_ai_juniemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiJuniemvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiJuniemvcApplication.class, args);
+	}
+
+}

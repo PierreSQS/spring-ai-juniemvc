7058df54ab61f8f1291bf7584eac2423fb569529
PierreSQS <nomo_messina@web.de>
2025-10-30 17:27:02 +0100

    Sec10_Chap77: created Beer JPA entity with fields and annotations

---
+++ src/main/java/guru/springframework/spring_ai_juniemvc/entities/Beer.java
@@ -0,0 +1,53 @@
+package guru.springframework.spring_ai_juniemvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+/**
+ * Beer JPA Entity. Created by Junie
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "beer")
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
+    private String beerName;
+
+    private String beerStyle;
+
+    private String upc;
+
+    private Integer quantityOnHand;
+
+    private BigDecimal price;
+
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime updateDate;
+}
